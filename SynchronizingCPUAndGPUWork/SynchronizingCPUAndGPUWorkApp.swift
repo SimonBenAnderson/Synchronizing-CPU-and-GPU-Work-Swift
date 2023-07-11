a33b2a0d1ba833a1513b0ed93619fa3a63c6e0ad
Simon Anderson <simonbenanderson@gmail.com>
2023-07-11 21:04:47 +1200

    Initial Commit

---
+++ SynchronizingCPUAndGPUWork/SynchronizingCPUAndGPUWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynchronizingCPUAndGPUWorkApp.swift
+//  SynchronizingCPUAndGPUWork
+//
+//  Created by Simon Anderson on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SynchronizingCPUAndGPUWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
